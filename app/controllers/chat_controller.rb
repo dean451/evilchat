95caab6d4baaded04944b7aebe8ed3107167dce1
Dean Palm <dean@sxsw.com>
2018-02-06 21:56:10 -0600

    component generators and messages oh my

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,2 +1,19 @@
 class ChatController < ApplicationController
+
+  before_action :authenticate!
+
+  # display last 20
+
+  def show
+    @messages = Message.order(created_at: :asc).last(20)
+  end
+
+  private
+
+  # redirect user to /login if no UN
+
+  def authenticate!
+    redirect_to login_path unless session[:username]
+  end
+
 end
--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -3,3 +3,4 @@
 import "init";
 import "components/page/page";
 import "components/auth-form/auth-form";
+import "components/chat/chat";
