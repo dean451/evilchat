a2906ca4b737eb14d5216299a5358674243f4f13
Dean Palm <dean@sxsw.com>
2018-02-12 21:59:09 -0600

    create channel consumer

---
+++ frontend/client/cable.js
@@ -0,0 +1,15 @@
+// frontend/client/cable.js
+
+import cable from "actioncable";
+
+let consumer;
+
+function createChannel(...args) {
+  if (!consumer) {
+    consumer = cable.createConsumer();
+  }
+
+  return consumer.subscriptions.create(...args);
+}
+
+export default createChannel;
